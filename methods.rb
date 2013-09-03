3453bc65ae23646f0053040404cf83ef3dcbe3e6
Guille Paz <guille87paz@gmail.com>
2013-09-03 19:56:30 -0300

    Create basic methods.

---
+++ methods.rb
@@ -0,0 +1,18 @@
+def friend(name):
+  puts "My friend is " + name + "."
+end
+
+def friend2(name):
+  puts "My friend is #{name}."
+end
+
+def add(a, b)
+    return a + b
+end
+
+# Splat arguments: "Hey Ruby, I don't know how many arguments there are about to be, but it could be more than one."
+def what_up(greeting, *bros)
+  bros.each { |bro| puts "#{greeting}, #{bro}!" }
+end
+
+what_up("What up", "Justin", "Ben", "Kevin Sorbo")
